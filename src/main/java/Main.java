8b6b536f75a7ff89c8febd756640f5a3e2ae3f5d
aijunkie <satenov@gmail.com>
2015-02-22 20:27:31 +0300

    fuck ya message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
-/**
- * Created by ThinkRay on 20.02.2015.
- */
+
 public class Main {
+    public static void main(String[] args) {
 
-
+    }
 }
--- src/main/java/matrix/matrix.java
+++ src/main/java/matrix/matrix.java
@@ -1,12 +1,25 @@
 package matrix;
 
-/**
- * Created by ThinkRay on 20.02.2015.
- */
 public class matrix {
     public static void main(String[] args) {
-        System.out.print("Hello Motherfucker!");
+        System.out.println("Hello Motherfucker!");
         System.out.println("bloody fuckin' hell!!!");
 
+
+        int rows = 3;
+        int columns = 5;
+
+        int[][] array = new int[rows][columns];
+
+        for(int i = 0; i<rows; i++)
+            for(int j = 0; j<columns; j++)
+                array[i][j] = 1;
+
+        for(int i = 0; i<rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
